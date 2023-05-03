b540a13fea8e75535a8137e90a5687010c4c770e
Krista M <morales-k@outlook.com>
2023-05-03 14:51:49 -0400

    Home page update
    Added images and lorum ipsum to home page.

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,25 @@
 
-import { View, Text} from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 import { globalStyles } from '../Styles/global';
 
 function Home() {
+  const bookImg = require('../assets/books.jpg');
+  const bookShelfImg = require('../assets/bookshelf.jpg');
 
   return (
-    <View style={globalStyles.container}>
-        <Text style={globalStyles.font20}>Home Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={globalStyles.container}>
+          <Text style={globalStyles.heading}>Books Plus</Text>
+          <Text style={globalStyles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+          <Image style={globalStyles.img} source={bookImg} />
+          <Text style={globalStyles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam quisque id diam vel quam elementum pulvinar etiam non. Ut diam quam nulla porttitor massa. Dignissim sodales ut eu sem integer vitae justo eget. Senectus et netus et malesuada fames ac turpis egestas.</Text>
+          <View style={globalStyles.container}>
+            <Text style={globalStyles.heading}>Books of every variety</Text>
+            <Image style={globalStyles.img} source={bookShelfImg} />
+            <Text style={globalStyles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam quisque id diam vel quam elementum pulvinar etiam non. Ut diam quam nulla porttitor massa. Dignissim sodales ut eu sem integer vitae justo eget.</Text>
+          </View>
+      </View>
+    </ScrollView>
   )
 }
 
